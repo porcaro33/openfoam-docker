ecac8c61eed34bb4c8c720f4911d22919b6a825e
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2017-03-28 22:03:43 +0900

    initial commit

---
+++ submit_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+QUEUE="openfoam-job-queue"
+JOBDEF="openfoam-docker.1"
+MAXITR=5
+
+for i in `seq 1 $MAXITR`
+do
+  JOBNAME="JOB$i"
+  aws batch submit-job --job-name $JOBNAME --job-queue $QUEUE --job-definition $JOBDEF
+  echo "submitted $JOBNAME"
+done
