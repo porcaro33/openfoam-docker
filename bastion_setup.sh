615a135a6ae173e9e1ea22b67c26fcdada26af9f
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2017-07-24 05:35:19 +0900

    add reference url

--- bastion_setup.sh
+++ bastion_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # setup docker-ce and awscli on Ubuntu Xenial 16.04 (LTS)
+# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-from-a-package
+# http://docs.aws.amazon.com/cli/latest/userguide/installing.html
 
 sudo apt-get remove -y docker docker-engine docker.io
 sudo apt-get update -y
