c0e30cbcdf991a70aac084d23d62acc614ecb569
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2017-07-26 02:25:40 +0900

    added all commands

--- commands.sh
+++ commands.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# select aws profile
-export AWS_DEFAULT_PROFILE=<your_profile>
-# or run aws configure and set your credential info
-aws configure
+# git clone
+git clone https://github.com/porcaro33/openfoam-docker.git
+
+# rnu setup script
+cd openfoam-docker
+./bastion_setup.sh
 
 # docker build
-docker build -t openfoam-batch:latest .
+sudo docker build -t openfoam-batch:latest .
 
 # push to ECR
 aws ecr get-login --no-include-email --region us-west-2
 # Run the docker login command that was returned in "ecr get-login" command
 sudo docker tag openfoam-batch:latest <account-id>.dkr.ecr.us-west-2.amazonaws.com/openfoam-batch:latest
 sudo docker push <account-id>.dkr.ecr.us-west-2.amazonaws.com/openfoam-batch:latest
+
+# edit json files
+sudo vi job_definition.json
+sudo vi computing_env.json
+sudo vi job_queue.json
 
 # job definition
 aws --region us-west-2 batch register-job-definition --cli-input-json file://job_definition.json
