b0e0e52b6fca457659cdaeae7765a45fbc4cdb8b
Hiroshi Kobayashi <hiroshi.kobayashi@wdc.com>
2017-07-25 17:01:23 +0900

    changed queue name

--- submit_batch.sh
+++ submit_batch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-QUEUE="openfoam-docker-queue"
-JOBDEF="openfoam-docker-job"
-MAXITR=10
+QUEUE="openfoam-batch-queue"
+JOBDEF="openfoam-batch-job"
+REGION="us-west-2"
+TIMES=10
 
-for i in `seq 1 $MAXITR`
+for i in `seq 1 $TIMES`
 do
   JOBNAME="JOB_$i"
-  aws batch submit-job --job-name $JOBNAME --job-queue $QUEUE --job-definition $JOBDEF
+  aws --region $REGION batch submit-job --job-name $JOBNAME --job-queue $QUEUE --job-definition $JOBDEF
   echo "submitted $JOBNAME"
   sleep 10
 done
